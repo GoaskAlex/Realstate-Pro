772148576c3d481c86d240727fe3f83620144e2e
John Rodriguez <johnmaverickox@gmail.com>
2024-10-07 16:52:28 -0400

    Created user model

---
+++ server/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    profilePicture:{
+        type:String,
+        required:false,
+        default:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn1.vectorstock.com%2Fi%2F1000x1000%2F42%2F80%2Fmaneki-neko-japanese-lucky-cat-fortune-symbol-vector-10214280.jpg&f=1&nofb=1&ipt=18aee661821c8561e8a32bb4fae6d6364b0a30ded8cbba69b5379fbb5f9ba771&ipo=images"
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    }
+},{timestamps}
+)
+
+const User = mongoose.model('User', userSchema)
+
+export default User
