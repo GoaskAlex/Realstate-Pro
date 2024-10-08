9edc812d692fa62eb7c6e6ebaef28123e1feabbf
John Rodriguez <johnmaverickox@gmail.com>
2024-10-08 13:40:11 -0400

    created signup server route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.routes.js'
+import authRouter from './routes/auth.routes.js'
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_ENV).then(()=>{
@@ -12,10 +13,12 @@ console.log(err);
 })
 
 const app = express()
+app.use(express.json())
 
 app.listen(3000,()=>{
     console.log('Server Running');
 })
 
 app.use("/server/user", userRouter)
+app.use("/server/auth", authRouter)
 
