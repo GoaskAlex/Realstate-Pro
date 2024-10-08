325c08be9b70d728da8c0c66567a81cd5c3e9cc2
John Rodriguez <johnmaverickox@gmail.com>
2024-10-08 16:29:39 -0400

    added error middle ware

--- server/index.js
+++ server/index.js
@@ -22,3 +22,12 @@ app.listen(3000,()=>{
 app.use("/server/user", userRouter)
 app.use("/server/auth", authRouter)
 
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error!'
+    res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode,
+    })
+})
